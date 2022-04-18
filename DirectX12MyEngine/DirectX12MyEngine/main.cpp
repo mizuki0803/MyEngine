0a749aad75aa5beb33b894a6eaf156f423792b74
miuzki0803 <k020g1157@g.neec.ac.jp>
2022-04-18 14:43:24 +0900

    FBX SDKの追加

--- DirectX12MyEngine/DirectX12MyEngine/main.cpp
+++ DirectX12MyEngine/DirectX12MyEngine/main.cpp
@@ -1,8 +1,11 @@
 #include "AndosGame.h"
+#include "fbxsdk.h"
 
 //Windowsアプリでのエントリーポイント（main関数）
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	FbxManager* fbxManager = FbxManager::Create();
+
 	FrameWork *game = new AndosGame();
 
 	game->Run();
