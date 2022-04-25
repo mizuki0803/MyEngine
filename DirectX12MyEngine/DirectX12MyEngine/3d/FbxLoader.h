c4621c5e966de6d9b690280c2b9bef0a8f8ebafd
mizuki0803 <k020g1157@g.neec.ac.jp>
2022-04-25 14:31:55 +0900

    FBX読み込みの階層構造を構築

--- DirectX12MyEngine/DirectX12MyEngine/3d/FbxLoader.h
+++ DirectX12MyEngine/DirectX12MyEngine/3d/FbxLoader.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "fbxsdk.h"
+#include "FbxModel.h"
 
 #include <d3d12.h>
 #include <d3dx12.h>
@@ -51,6 +52,14 @@ public:
 	/// </summary>
 	/// <param name="modelName">モデル名</param>
 	void LoadModelFromFile(const string& modelName);
+
+	/// <summary>
+	/// 再帰的にノード構成を解析
+	/// </summary>
+	/// <param name="fbxModel">読み込み先モデルオブジェクト</param>
+	/// <param name="fbxNode">解析対象のノード</param>
+	/// <param name="parent">親ノード</param>
+	void ParseNodeRecursive(FbxModel* fbxModel, FbxNode* fbxNode, Node* parent = nullptr);
 
 private:
 	//デバイス
